50a063efbd760e7a268b35ce5a88621bb8502ed2
guddu69 <122154065+guddu69@users.noreply.github.com>
2023-09-11 03:16:53 +0530

    Update rserver.c

--- TCP chat-bot/rserver.c
+++ TCP chat-bot/rserver.c
@@ -4,57 +4,58 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 
+#include<unistd.h>
 #include<netinet/in.h>
 
-#include <unistd.h> // IMP added afterwards for mac users
-#include <string.h>
+#include<string.h>
+// create -> address -> bind -> listen -> accept -> while -> close
 int main(){
-
+    
     // creating socket
     int socket_n = socket(AF_INET, SOCK_STREAM, 0);
 
-    // defining socket address
+    // defining server address
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(9002);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    // binding to specific port number and IP
-    bind(socket_n, (struct sockaddr*) &server_address, sizeof(server_address));
-    listen(socket_n, 5); // listining for any connection
-
-    // accepting the connection
+    // binding socket to port number and IP
+    bind(socket_n, (struct sockaddr*)&server_address, sizeof(server_address));
+    listen(socket_n, 5);
     int client_n = accept(socket_n, NULL, NULL);
 
-   while (1){
-        // recieving message from client
+    while(1){
+
+        // RECEVING
+
         char rmsg[256];
         recv(client_n, &rmsg, sizeof(rmsg), 0);
 
-        printf("Msg from client: %s",rmsg);
+        printf("CLIENT: %s", rmsg);
 
-        if (strcmp(rmsg, "exit\n")==0)
-        {
+        if (strcmp(rmsg, "exit\n")==0){
+            printf("EXITING...");
             break;
         }
 
-        char smsg[256];
+        // SENDING
+
         printf("Reply: ");
-        fgets(smsg , sizeof(smsg), stdin); // taking input from user
-        send(client_n, smsg, sizeof(smsg), 0 );
-
-        if (strcmp(smsg, "exit\n")==0)
-        {
+        char smsg[256];
+        fgets(smsg, sizeof(smsg), stdin);
+        send(client_n, smsg, sizeof(smsg), 0);
+        
+        if (strcmp(smsg, "exit\n")==0){
+            printf("EXITING...");
             break;
         }
 
-   }
-   
+    }
 
-    // closing both sockets
     close(client_n);
     close(socket_n);
-
+    
     return 0;
 
 }
