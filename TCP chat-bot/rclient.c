20304e99e213beddeec41ab41f985b990affa415
guddu69 <122154065+guddu69@users.noreply.github.com>
2023-09-11 03:18:55 +0530

    Update rclient.c

--- TCP chat-bot/rclient.c
+++ TCP chat-bot/rclient.c
@@ -4,54 +4,58 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 
+#include<unistd.h>
 #include<netinet/in.h>
-#include <unistd.h> // IMP added afterwards for mac users
-#include <string.h>
+
+#include<string.h>
+// create -> address -> connect -> while -> close
 int main(){
-    
+ 
     // creating socket
-    int socket_n = socket(AF_INET, SOCK_STREAM,0);
+    int socket_n = socket(AF_INET, SOCK_STREAM, 0);
 
-    // allocating address
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(9002);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    // defining client address
+    struct sockaddr_in client_address;
+    client_address.sin_family = AF_INET;
+    client_address.sin_port = htons(9002);
+    client_address.sin_addr.s_addr = INADDR_ANY;
 
-    // making connection
-    int connection = connect(socket_n, (struct sockaddr*) &server_address, sizeof(server_address));
+    // making connection to the servet
+    int connection = connect(socket_n, (struct sockaddr*)&client_address, sizeof(client_address));
 
     if (connection == -1){
-        printf("Error occured\n");
+        printf("error occured during connection\n");
     }
+    
+    while(1){
 
-   while(1){
-        // sending message to server
+        // SENDING
+
         char smsg[256];
-        printf("Enter the message: ");
-        fgets(smsg , sizeof(smsg), stdin); // taking input from user
-        send(socket_n, smsg, sizeof(smsg), 0 );
+        printf("Enter msg: ");
+        fgets(smsg, sizeof(smsg), stdin);
+        send(socket_n, smsg, sizeof(smsg), 0);
 
-        if (strcmp(smsg, "exit\n")==0)
-        {
+        if(strcmp(smsg,"exit\n")==0){
+            printf("EXITING...");
             break;
         }
 
-        // receving message from client
+        // RECEVING
+
         char rmsg[256];
         recv(socket_n, &rmsg, sizeof(rmsg), 0);
 
-        // printing msg
-        printf("Recieved: %s", rmsg);
-
-        if (strcmp(rmsg, "exit\n")==0)
-        {
+        printf("SERVER: %s", rmsg);
+        if (strcmp(rmsg, "exit\n")==0){
+            printf("EXITING...");
             break;
         }
-   }
 
-    // closing socket
+    }
+
     close(socket_n);
 
     return 0;
+
 }
