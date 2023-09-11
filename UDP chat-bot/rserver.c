50abc990660d6c28cabcb4c2da18c28a521baf98
guddu69 <122154065+guddu69@users.noreply.github.com>
2023-09-11 12:21:02 +0530

    Update rserver.c

--- UDP chat-bot/rserver.c
+++ UDP chat-bot/rserver.c
@@ -4,49 +4,44 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 
+#include<unistd.h>
 #include<netinet/in.h>
 
-#include <unistd.h> // IMP added afterwards for mac users
-#include<string.h> 
-#define PORT 8080
+#include<string.h>
+
 int main(){
 
-    // creating socket
     int socket_n = socket(AF_INET, SOCK_DGRAM, 0);
 
-    // defining socket address
-    struct sockaddr_in server_address;
+    struct sockaddr_in server_address, client_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
+    server_address.sin_port = htons(8080);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
+    bind(socket_n, (struct sockaddr*)&server_address, sizeof(server_address));
+
     while(1){
-        // binding to specific port number and IP
-        bind(socket_n, (struct sockaddr*) &server_address, sizeof(server_address));
 
-        struct sockaddr_in client_addr;
-
-        // recieving message from client
         char rmsg[256];
-        socklen_t client_n_len = sizeof(client_addr);
-        ssize_t bytes_rev = recvfrom(socket_n, rmsg, sizeof(rmsg), 0, (struct sockaddr*)&client_addr, &client_n_len);
+        socklen_t len = sizeof(client_address);
+        ssize_t bytes_recv = recvfrom(socket_n, rmsg, sizeof(rmsg), 0, (struct sockaddr*)&client_address, &len);
 
-        rmsg[bytes_rev] = '\0'; // Null-terminate the received message
-        if (strcmp(rmsg, "exit\n")==0){
+        rmsg[bytes_recv] = '\0';
+        printf("Client: %s", rmsg);
+
+        if(strcmp(rmsg, "exit\n")==0)
             break;
-        }
 
-        // sending the message
         printf("Reply: ");
         char smsg[256];
-        fgets(smsg , sizeof(smsg), stdin); // taking input from user
-        ssize_t bytes_sent = sendto(socket_n, smsg, bytes_rev, 0, (struct sockaddr*)&client_addr, sizeof(client_addr));
-        
-        if (strcmp(smsg, "exit\n")==0){
+        fgets(smsg, sizeof(smsg), stdin);
+        sendto(socket_n, smsg, bytes_recv, 0, (struct sockaddr*)&client_address, sizeof(client_address));
+
+        if(strcmp(smsg, "exit\n")==0)
             break;
-        }
+
     }
-    // closing sockets
+
     close(socket_n);
 
     return 0;
